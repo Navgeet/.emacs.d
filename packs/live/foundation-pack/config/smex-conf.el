e89b28b78b75d7926af5cc48e075d3f3083c809f
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-05-06 21:01:48 +0530

    smex-conf.el: Ignore menu-bar advice

--- packs/live/foundation-pack/config/smex-conf.el
+++ packs/live/foundation-pack/config/smex-conf.el
@@ -3,5 +3,6 @@
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(setq smex-key-advice-ignore-menu-bar t)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
