eb8acb6b768c306d9eb9cb91770350c3a353d859
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-05-06 21:10:26 +0530

    foundation-pack/init.el: Disable some unneeded modes

--- packs/live/foundation-pack/init.el
+++ packs/live/foundation-pack/init.el
@@ -4,7 +4,7 @@
 
 (require 'dircolors)
 (require 'smooth-scrolling)
-(require 'buffer-move)
+;; (require 'buffer-move)
 (require 'find-file-in-project)
 
 (live-load-config-file "backup-dir-conf.el")
@@ -15,17 +15,17 @@
 (live-load-config-file "smex-conf.el")
 (live-load-config-file "tramp-conf.el")
 (live-load-config-file "mouse-conf.el")
-(live-load-config-file "ibuffer-git-conf.el")
-(live-load-config-file "key-chord-conf.el")
-(live-load-config-file "window-number-conf.el")
+;; (live-load-config-file "ibuffer-git-conf.el")
+;; (live-load-config-file "key-chord-conf.el")
+;; (live-load-config-file "window-number-conf.el")
 (live-load-config-file "recentf-conf.el")
 (live-load-config-file "elisp-slime-nav-conf.el")
 (live-load-config-file "popwin-conf.el")
-(live-load-config-file "shell-conf.el")
+;; (live-load-config-file "shell-conf.el")
 (live-load-config-file "spelling-conf.el")
 (live-load-config-file "lisp-conf.el")
-(live-load-config-file "win-switch-conf.el")
-(live-load-config-file "zone-conf.el")
+;; (live-load-config-file "win-switch-conf.el")
+;; (live-load-config-file "zone-conf.el")
 (live-load-config-file "monkey-patch.el")
 
 
