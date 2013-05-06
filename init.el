29d93a47df6bbbc6d3ffcddba962036b827fb094
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-05-06 20:59:27 +0530

    init.el: Add bootstrap.el

--- init.el
+++ init.el
@@ -1,35 +1,7 @@
 
+;; Create tmp dirs and initialize dir vars
+(load-file (concat user-emacs-directory "bootstrap-dirs.el"))
 
-(let ((emacs-live-directory (getenv "EMACS_LIVE_DIR")))
-  (when emacs-live-directory
-    (setq user-emacs-directory emacs-live-directory)))
-
-(when live-supported-emacsp
-;; Store live base dirs, but respect user's choice of `live-root-dir'
-;; when provided.
-(setq live-root-dir (if (boundp 'live-root-dir)
-                        (file-name-as-directory live-root-dir)
-                      user-emacs-directory))
-
-(setq
- live-tmp-dir      (file-name-as-directory (concat live-root-dir "tmp"))
- live-etc-dir      (file-name-as-directory (concat live-root-dir "etc"))
- live-pscratch-dir (file-name-as-directory (concat live-tmp-dir  "pscratch"))
- live-lib-dir      (file-name-as-directory (concat live-root-dir "lib"))
- live-packs-dir    (file-name-as-directory (concat live-root-dir "packs"))
- live-autosaves-dir(file-name-as-directory (concat live-tmp-dir  "autosaves"))
- live-backups-dir  (file-name-as-directory (concat live-tmp-dir  "backups"))
- live-custom-dir   (file-name-as-directory (concat live-etc-dir  "custom"))
- live-load-pack-dir nil
- live-disable-zone nil)
-
-;; create tmp dirs if necessary
-(make-directory live-etc-dir t)
-(make-directory live-tmp-dir t)
-(make-directory live-autosaves-dir t)
-(make-directory live-backups-dir t)
-(make-directory live-custom-dir t)
-(make-directory live-pscratch-dir t)
 (defun live-version ()
   "1.0beta22")
 
