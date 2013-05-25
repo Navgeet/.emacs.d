6ed5ca3ceb88385b50c1c0f9c31aa98526f16248
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-05-25 10:12:17 +0530

    sc-conf.el: put sr-goto-dir on ido steroids

--- packs/live/file-management-pack/config/sc-conf.el
+++ packs/live/file-management-pack/config/sc-conf.el
@@ -1,2 +1,8 @@
 (live-add-pack-lib "sunrise-commander")
 (require 'sunrise-commander)
+
+(defun nav/sr-goto-dir ()
+  (interactive)
+  (sr-goto-dir (ido-read-directory-name "cd: ")))
+
+(define-key sr-mode-map (kbd "/") 'nav/sr-goto-dir)
