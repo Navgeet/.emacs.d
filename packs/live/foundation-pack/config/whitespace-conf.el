b09654938a0850206e50400a744f6859e142c067
Navgeet Agrawal <navgeet.agrawal@gmail.com>
2013-05-27 05:41:11 +0530

    New: foundation-pack/whitespace-conf.el

---
+++ packs/live/foundation-pack/config/whitespace-conf.el
@@ -0,0 +1,8 @@
+(require 'whitespace)
+
+(setq whitespace-line-column 80
+      whitespace-style '(face
+                         trailing lines-tail indentation
+                         space-before-tab space-after-tab))
+
+(global-whitespace-mode 1)
